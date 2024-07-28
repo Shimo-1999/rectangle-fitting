cc67a5fc84878f26ca2cf1c6756778f9dc9fbf9d
Yuki Shimoyamada <azsedcftgbhujmkol0427@gmail.com>
2024-07-28 23:56:26 +0900

    Optimize: Improve processing speed using BinaryHeap

--- wasm_project/src/algorithms/greedy.rs
+++ wasm_project/src/algorithms/greedy.rs
@@ -1,6 +1,7 @@
-use wasm_bindgen::prelude::*;
+use std::collections::BinaryHeap;
+use svg::node::element::Rectangle as SvgRectangle;
 use svg::Document;
-use svg::node::element::Rectangle;
+use wasm_bindgen::prelude::*;
 
 struct RGBGrid(Vec<Vec<(u64, u64, u64)>>);
 impl RGBGrid {
@@ -31,6 +32,130 @@ impl RGBGrid {
     }
 }
 
+#[derive(Clone, Copy, Eq, PartialEq)]
+enum SplitLine {
+    Horizontal(usize), // 1..height
+    Vertical(usize),   // 1..width
+}
+
+#[derive(Clone, Copy, Eq, PartialEq)]
+struct Rectangle {
+    top: usize,
+    left: usize,
+    bottom: usize,
+    right: usize,
+}
+
+impl Rectangle {
+    #[rustfmt::skip]
+    fn find_best_split(&self, rgb_grid: &RGBGrid, rgb_squared_grid: &RGBGrid) -> (SplitLine, u64) {
+        let mut best_split_line = SplitLine::Horizontal(self.top);
+        let mut best_cost = 0;
+        let (r_sum, g_sum, b_sum) = rgb_grid.get_sum(self.top, self.left, self.bottom, self.right);
+        let (r_square_sum, g_square_sum, b_square_sum) = rgb_squared_grid.get_sum(self.top, self.left, self.bottom, self.right);
+        let area = ((self.bottom - self.top) * (self.right - self.left)) as u64;
+        let original_cost = r_square_sum - r_sum.pow(2) / area + g_square_sum - g_sum.pow(2) / area + b_square_sum - b_sum.pow(2) / area;
+        // evaluate horizontal splits
+        for y in self.top + 1..self.bottom {
+            // top rectangle
+            let (r_sum_top, g_sum_top, b_sum_top) = rgb_grid.get_sum(self.top, self.left, y, self.right);
+            let (r_square_sum_top, g_square_sum_top, b_square_sum_top) = rgb_squared_grid.get_sum(self.top, self.left, y, self.right);
+            let area_top = ((y - self.top) * (self.right - self.left)) as u64;
+            let top_cost = r_square_sum_top - r_sum_top.pow(2) / area_top + g_square_sum_top - g_sum_top.pow(2) / area_top + b_square_sum_top - b_sum_top.pow(2) / area_top;
+            // bottom rectangle
+            let (r_sum_bottom, g_sum_bottom, b_sum_bottom) = rgb_grid.get_sum(y, self.left, self.bottom, self.right);
+            let (r_square_sum_bottom, g_square_sum_bottom, b_square_sum_bottom) = rgb_squared_grid.get_sum(y, self.left, self.bottom, self.right);
+            let area_bottom = ((self.bottom - y) * (self.right - self.left)) as u64;
+            let bottom_cost = r_square_sum_bottom - r_sum_bottom.pow(2) / area_bottom + g_square_sum_bottom - g_sum_bottom.pow(2) / area_bottom + b_square_sum_bottom - b_sum_bottom.pow(2) / area_bottom;
+
+            if original_cost - (top_cost + bottom_cost) > best_cost {
+                best_cost = original_cost - (top_cost + bottom_cost);
+                best_split_line = SplitLine::Horizontal(y);
+            }
+        }
+        // evaluate vertical splits
+        for x in self.left + 1..self.right {
+            // left rectangle
+            let (r_sum_left, g_sum_left, b_sum_left) = rgb_grid.get_sum(self.top, self.left, self.bottom, x);
+            let (r_square_sum_left, g_square_sum_left, b_square_sum_left) = rgb_squared_grid.get_sum(self.top, self.left, self.bottom, x);
+            let area_left = ((self.bottom - self.top) * (x - self.left)) as u64;
+            let left_cost = r_square_sum_left - r_sum_left.pow(2) / area_left + g_square_sum_left - g_sum_left.pow(2) / area_left + b_square_sum_left - b_sum_left.pow(2) / area_left;
+            // right rectangle
+            let (r_sum_right, g_sum_right, b_sum_right) = rgb_grid.get_sum(self.top, x, self.bottom, self.right);
+            let (r_square_sum_right, g_square_sum_right, b_square_sum_right) = rgb_squared_grid.get_sum(self.top, x, self.bottom, self.right);
+            let area_right = ((self.bottom - self.top) * (self.right - x)) as u64;
+            let right_cost = r_square_sum_right - r_sum_right.pow(2) / area_right + g_square_sum_right - g_sum_right.pow(2) / area_right + b_square_sum_right - b_sum_right.pow(2) / area_right;
+
+            if original_cost - (left_cost + right_cost) > best_cost {
+                best_cost = original_cost - (left_cost + right_cost);
+                best_split_line = SplitLine::Vertical(x);
+            }
+        }
+        (best_split_line, best_cost)
+    }
+    fn split(&self, split_line: SplitLine) -> (Rectangle, Rectangle) {
+        match split_line {
+            SplitLine::Horizontal(y) => (
+                Rectangle {
+                    top: self.top,
+                    left: self.left,
+                    bottom: y,
+                    right: self.right,
+                },
+                Rectangle {
+                    top: y,
+                    left: self.left,
+                    bottom: self.bottom,
+                    right: self.right,
+                },
+            ),
+            SplitLine::Vertical(x) => (
+                Rectangle {
+                    top: self.top,
+                    left: self.left,
+                    bottom: self.bottom,
+                    right: x,
+                },
+                Rectangle {
+                    top: self.top,
+                    left: x,
+                    bottom: self.bottom,
+                    right: self.right,
+                },
+            ),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+struct RectangleSplit {
+    rectangle: Rectangle,
+    split_line: SplitLine,
+    cost_decrease: u64,
+}
+
+impl RectangleSplit {
+    fn new(rectangle: Rectangle, split_line: SplitLine, cost_decrease: u64) -> Self {
+        Self {
+            rectangle,
+            split_line,
+            cost_decrease,
+        }
+    }
+}
+
+impl Ord for RectangleSplit {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.cost_decrease.cmp(&other.cost_decrease)
+    }
+}
+
+impl PartialOrd for RectangleSplit {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 #[wasm_bindgen]
 pub fn run_greedy(num_rectangles: usize, width: usize, height: usize, rgb_data: &[u8]) -> Vec<String> {
     let rgb_data = rgb_data.iter().map(|&x| x as u64).collect::<Vec<u64>>();
@@ -41,103 +166,44 @@ pub fn run_greedy(num_rectangles: usize, width: usize, height: usize, rgb_data: 
 
     let mut animation_data = vec![];
 
-    let mut rectangles = vec![(0, 0, height, width)]; // top-left, bottom-right
-    for _ in 0..num_rectangles {
-        let mut best_cost = 0;
-        let mut best_idx = 0;
-        let mut best_yx = (0, 0);
-        for (idx, &(top, left, bottom, right)) in rectangles.iter().enumerate() {
-            let (r_sum, g_sum, b_sum) = rgb_grid.get_sum(top, left, bottom, right);
-            let (r_square_sum, g_square_sum, b_square_sum) = rgb_grid_squared.get_sum(top, left, bottom, right);
-            // original rectangle
-            let area = ((bottom - top) * (right - left)) as f64;
-            let mut original_cost = 0;
-            original_cost += r_square_sum - (r_sum.pow(2) as f64 / area).round() as u64;
-            original_cost += g_square_sum - (g_sum.pow(2) as f64 / area).round() as u64;
-            original_cost += b_square_sum - (b_sum.pow(2) as f64 / area).round() as u64;
+    let initial_rectangle = Rectangle {
+        top: 0,
+        left: 0,
+        bottom: height,
+        right: width,
+    };
+    let (split_line, cost_decrease) = initial_rectangle.find_best_split(&rgb_grid, &rgb_grid_squared);
+    let mut rectangles_heap = BinaryHeap::new();
+    rectangles_heap.push(RectangleSplit::new(initial_rectangle, split_line, cost_decrease));
 
-            for y in top + 1..bottom {
-                // top rectangle
-                let (r_sum, g_sum, b_sum) = rgb_grid.get_sum(top, left, y, right);
-                let (r_square_sum, g_square_sum, b_square_sum) = rgb_grid_squared.get_sum(top, left, y, right);
-                let area = ((y - top) * (right - left)) as f64;
-                let mut top_cost = 0;
-                top_cost += r_square_sum - (r_sum.pow(2) as f64 / area).round() as u64;
-                top_cost += g_square_sum - (g_sum.pow(2) as f64 / area).round() as u64;
-                top_cost += b_square_sum - (b_sum.pow(2) as f64 / area).round() as u64;
-                // bottom rectangle
-                let (r_sum, g_sum, b_sum) = rgb_grid.get_sum(y, left, bottom, right);
-                let (r_square_sum, g_square_sum, b_square_sum) = rgb_grid_squared.get_sum(y, left, bottom, right);
-                let area = ((bottom - y) * (right - left)) as f64;
-                let mut bottom_cost = 0;
-                bottom_cost += r_square_sum - (r_sum.pow(2) as f64 / area).round() as u64;
-                bottom_cost += g_square_sum - (g_sum.pow(2) as f64 / area).round() as u64;
-                bottom_cost += b_square_sum - (b_sum.pow(2) as f64 / area).round() as u64;
-
-                if original_cost - (top_cost + bottom_cost) > best_cost {
-                    best_cost = original_cost - (top_cost + bottom_cost);
-                    best_idx = idx;
-                    best_yx = (y, 0);
-                }
-            }
-            for x in left + 1..right {
-                // left rectangle
-                let (r_sum, g_sum, b_sum) = rgb_grid.get_sum(top, left, bottom, x);
-                let (r_square_sum, g_square_sum, b_square_sum) = rgb_grid_squared.get_sum(top, left, bottom, x);
-                let area = ((bottom - top) * (x - left)) as f64;
-                let mut left_cost = 0;
-                left_cost += r_square_sum - (r_sum.pow(2) as f64 / area).round() as u64;
-                left_cost += g_square_sum - (g_sum.pow(2) as f64 / area).round() as u64;
-                left_cost += b_square_sum - (b_sum.pow(2) as f64 / area).round() as u64;
-                // right rectangle
-                let (r_sum, g_sum, b_sum) = rgb_grid.get_sum(top, x, bottom, right);
-                let (r_square_sum, g_square_sum, b_square_sum) = rgb_grid_squared.get_sum(top, x, bottom, right);
-                let area = ((bottom - top) * (right - x)) as f64;
-                let mut right_cost = 0;
-                right_cost += r_square_sum - (r_sum.pow(2) as f64 / area).round() as u64;
-                right_cost += g_square_sum - (g_sum.pow(2) as f64 / area).round() as u64;
-                right_cost += b_square_sum - (b_sum.pow(2) as f64 / area).round() as u64;
-
-                if original_cost - (left_cost + right_cost) > best_cost {
-                    best_cost = original_cost - (left_cost + right_cost);
-                    best_idx = idx;
-                    best_yx = (0, x);
-                }
-            }
-        }
-
-        let (top, left, bottom, right) = rectangles[best_idx]; // old rectangle
-        match best_yx {
-            (y, 0) => {
-                rectangles[best_idx] = (top, left, y, right); // splitted top rectangle
-                rectangles.push((y, left, bottom, right)); // splitted bottom rectangle
-            }
-            (0, x) => {
-                rectangles[best_idx] = (top, left, bottom, x); // splitted left rectangle
-                rectangles.push((top, x, bottom, right)); // splitted right rectangle
-            }
-            _ => (), // reachable, but no action needed as the rectangle is fully splitted
-        }
+    while rectangles_heap.len() <= num_rectangles {
+        let RectangleSplit {
+            rectangle,
+            split_line,
+            cost_decrease: _,
+        } = rectangles_heap.pop().unwrap();
+        let (rectangle1, rectangle2) = rectangle.split(split_line);
+        let (split_line1, cost_decrease1) = rectangle1.find_best_split(&rgb_grid, &rgb_grid_squared);
+        let (split_line2, cost_decrease2) = rectangle2.find_best_split(&rgb_grid, &rgb_grid_squared);
+        rectangles_heap.push(RectangleSplit::new(rectangle1, split_line1, cost_decrease1));
+        rectangles_heap.push(RectangleSplit::new(rectangle2, split_line2, cost_decrease2));
 
         let mut svg = Document::new()
             .set("viewBox", (-5, -5, width + 5, height + 5))
             .set("width", width)
             .set("height", height);
-        for &(top, left, bottom, right) in rectangles.iter() {
+        for &rectangle_split in rectangles_heap.iter() {
+            let Rectangle { top, left, bottom, right } = rectangle_split.rectangle;
             let (r_sum, g_sum, b_sum) = rgb_grid.get_sum(top, left, bottom, right);
-            let area = ((bottom - top) * (right - left)) as f64;
-            let r = (r_sum as f64 / area).round() as u8;
-            let g = (g_sum as f64 / area).round() as u8;
-            let b = (b_sum as f64 / area).round() as u8;
+            let area = ((bottom - top) * (right - left)) as u64;
+            let fill_color = format!("rgb({},{},{})", (r_sum / area) as u8, (g_sum / area) as u8, (b_sum / area) as u8);
             svg = svg.add(
-                Rectangle::new()
+                SvgRectangle::new()
                     .set("y", top)
                     .set("x", left)
                     .set("height", bottom - top)
                     .set("width", right - left)
-                    .set("fill", format!("rgb({}, {}, {})", r, g, b).as_str())
-                    // .set("stroke", "black")
-                    // .set("stroke-width", 1),
+                    .set("fill", fill_color), // .set("stroke", "black").set("stroke-width", 1),
             )
         }
         animation_data.push(svg.to_string());
